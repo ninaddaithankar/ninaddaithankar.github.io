6461846566a2c2c8c319648d9ce40e47e7a0d247
Ninad Daithankar <ninad.nd15@gmail.com>
2025-06-30 16:12:57 -0500

    made some style and font changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Footer: React.FC = () => {
   return (
     <div className="footer">
-      <p> © 2025 Ninad Daithankar</p>
-      <p>
-        Made with love and heart ♥️
-      </p>
       <p className="last-updated">
         Last updated: {new Date().toLocaleDateString()}
       </p>
+      <p>
+        Made with love, heart and cursor! ♥️
+      </p>
+      <p> © 2025 Ninad Daithankar</p>
     </div>
   )
 }
