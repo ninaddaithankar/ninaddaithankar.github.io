dce4ff1c3a501bd4d71cd9b1374ca6b3bc7d96c8
Ninad Daithankar <ninad.nd15@gmail.com>
2025-06-30 15:59:51 -0500

    handled media queries correctly

--- src/components/Misc.tsx
+++ src/components/Misc.tsx
@@ -8,11 +8,10 @@ const Misc: React.FC = () => {
       
       <ul className="misc-list">
         <li className="running-li">
-          Recently, I got into long distance running, and I absolutely love it. I am training for a sub-60 minute 10km finish. 
-          Stay tuned for a blog on my experience as a beginner runner and how it's rewiring my brain.
+          Long-distance running has become my new obsession. Morning runs with the cool air feel like meditation in motion. I'm pushing for a sub-60 minute 10K. Soon, I'll be writing about how running rewires the brain.
         </li>
         <li className="cricket-li">
-          I grew up playing Cricket. I was a part of my school's winning U-12, U-14 interschool cricket team. I also played for eQ's Cricket Team for 2 years during my time there.
+          I grew up playing Cricket. I was a part of my school's winning U-12, U-14 interschool cricket team. I also played for eQ's Cricket Team as a batting all-rounder for 2 years during my time there.
         </li>
         <li className="piano-li">
           Yiruma's "River Flows in You" inspired me to self learn piano during my undergrad. I also learned to play Bansuri (Indian Flute). I love listening to Indian and Western Classical Music. Favorites include Pt. Bhimsen Joshi, Pt. Hariprasad Chaurasia, Schubert, Beethoven and film scores from Hans Zimmer.
